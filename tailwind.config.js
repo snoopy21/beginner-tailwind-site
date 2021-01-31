7b7b80f83cb08e86ed1d1c8d72e0240bc998a584
martin <martin@vilan.ch>
2021-01-31 18:13:36 +0100

    colors teal und orange aktiviert
    Signed-off-by: martin <martin@vilan.ch>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ["src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+      colors: {
+          teal: colors.teal,
+          orange: colors.orange,
+      }
   },
   variants: {
     extend: {},
